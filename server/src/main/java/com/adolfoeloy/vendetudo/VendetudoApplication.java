efe31ee06f5adace3d7dcba610493217ca6846d7
Adolfo Eloy <1496547+adolfoweloy@users.noreply.github.com>
2018-08-21 11:49:18 -0300

    client and server app

---
+++ server/src/main/java/com/adolfoeloy/vendetudo/VendetudoApplication.java
@@ -0,0 +1,12 @@
+package com.adolfoeloy.vendetudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendetudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendetudoApplication.class, args);
+	}
+}
---
+++ server/src/test/java/com/adolfoeloy/vendetudo/VendetudoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.adolfoeloy.vendetudo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VendetudoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
