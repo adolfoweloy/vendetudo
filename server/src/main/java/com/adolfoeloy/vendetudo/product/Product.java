cbcbe59e1c55110507fef4059e0d20547cacb8bb
Adolfo Eloy <1496547+adolfoweloy@users.noreply.github.com>
2018-08-22 15:20:06 -0300

    configuring routes and link stylesheet

--- server/src/main/java/com/adolfoeloy/vendetudo/product/Product.java
+++ server/src/main/java/com/adolfoeloy/vendetudo/product/Product.java
@@ -4,14 +4,12 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.math.BigInteger;
-
 @Data
 @Document
 public class Product {
 
     @Id
-    private BigInteger id;
+    private String id;
     private String title;
     private String description;
     private double price;
